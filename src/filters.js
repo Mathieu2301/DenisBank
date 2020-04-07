bf142a328ade5866f863b0a239a651c87c99fa23
BuildTools <mathieu@usp-3.fr>
2020-04-07 17:56:01 +0200

    Add "round" and "add_plus" filters

--- src/filters.js
+++ src/filters.js
@@ -7,6 +7,8 @@ export default {
 //   fromNow: date => moment(date).add(2, 'hours').fromNow(),
 //   formatDate: date => `${this.addZeros(date.getDate())}/${this.addZeros(date.getMonth() - 1)}
 // à ${this.addZeros(date.getHours())}h${this.addZeros(date.getMinutes())}`,
+  round: (val, i = 1) => Math.round(val * (10 ** i)) / (10 ** i),
   addot: val => (val.includes(',') ? val : `${val},00`),
+  add_plus: val => ((typeof val === 'number' ? val >= 0 : !val.includes('-')) ? `+${val}` : `${val}`),
   bignbr: Intl.NumberFormat().format,
 };
